ae6eb725b4f7a4c4b162632315260766212f17c3
Campos2002 <campereira91@gmail.com>
2022-12-07 16:55:38 -0300

    continuação da produção do Header

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-// change nav style on scroll
+// change NAV style on scroll
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 });
+
+//Circular Contact Buttons
+const textButtons = document.querySelectorAll('.contact__btn');
+
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector('p');
+
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join()
+})
