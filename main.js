546d1db740b1133259f39cbbe9528dac110937fd
Campos2002 <campereira91@gmail.com>
2022-12-16 19:56:28 -0300

    inicio da seção Exhibitions

--- main.js
+++ main.js
@@ -3,11 +3,27 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 });
 
-//Circular Contact Buttons
-const textButtons = document.querySelectorAll('.contact__btn');
+//Swiper JS
 
-textButtons.forEach(textButton => {
-    let text = textButton.querySelector('p');
-
-    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
-})
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        599: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        999: {
+            slidesPerView: 3,
+            spaceBetween: 60
+        },
+        1079: {
+            slidesPerView: 4,
+            spaceBetween: 45
+        }
+    }
+});
