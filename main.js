aac8c60a6ff30a1c805176d5bed1818d5e58027d
Campos2002 <campereira91@gmail.com>
2022-12-22 16:39:07 -0300

    criação do código no script js para a navbar responsiva e continuaação da estilização no arquivo mediaQueries.css

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-// change NAV style on scroll
+// Change Nanvbar style on scroll.
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 });
 
-//Swiper JS
+//Responsive Navbar.
+const nav = document.querySelector('.nav__links');
+const openNavBtn = document.querySelector('#nav__toggle-open');
+const closeNavBtn = document.querySelector('#nav__toggle-close');
 
+const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+};
+
+const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+};
+
+openNavBtn.addEventListener('click', openNav);
+closeNavBtn.addEventListener('click', closeNav);
+nav.querySelectorAll('li a').forEach(navLink => { navLink.addEventListener('click', closeNav) })
+
+//Swiper JS.
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 20,
